8a703d381a46e42e30839f21875ddf43e93c9705
identu <angle276305@outlook.com>
2023-05-29 20:27:37 +0800

    Changes

---
+++ parentProj/consumer10000/src/main/java/org/exp/feign/UserFeignService.java
@@ -0,0 +1,9 @@
+package org.exp.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+@FeignClient("provider11000")
+public interface UserFeignService {
+    @GetMapping("/hello/say")
+    String Hello();
+}
