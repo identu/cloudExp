50842c6ca4a162ca0034433b42015ac6692d7fe7
identu <angle276305@outlook.com>
2023-06-01 00:29:47 +0800

    第五次实验，添加配置中心

--- parentProj/provider11000/src/main/java/org/exp/controller/HelloController.java
+++ parentProj/provider11000/src/main/java/org/exp/controller/HelloController.java
@@ -1,15 +1,19 @@
 package org.exp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@RefreshScope
 public class HelloController {
-
+    @Value("${word}")
+    private String word;
     @GetMapping("/say")
     public String Hello(){
-        return "hello world! there is port 11000!";
+        return "hello world! there is port 11000! "+word;
     }
 }
